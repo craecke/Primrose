d57ede6320aaccd41c743f7716d4cf286dc67406
Cody Russell <bratsche@gnome.org>
2011-06-04 11:37:25 -0500

    Average the final final judge scores.

--- app/models/score_list.rb
+++ app/models/score_list.rb
@@ -1,4 +1,33 @@
 class ScoreList < ActiveRecord::Base
   belongs_to :performer
   belongs_to :round
+
+  def scores
+    s = read_attribute(:scores)
+
+    s.split(" ").collect { |s| s.to_f }
+  end
+
+  def dropped_scores
+    d = read_attribute(:dropped_scores)
+    d.split(" ").collect { |d| d.to_i }
+  end
+
+  def final_scores
+    s = scores.clone
+    d = dropped_scores.sort.reverse
+
+    d.each { |i| s.delete_at(i) }
+    s
+  end
+
+  def average_all
+    a = scores
+    a.inject { |sum,el| sum + el }.to_f / a.size
+  end
+
+  def average_final
+    a = final_scores
+    a.inject { |sum,el| sum + el }.to_f / a.size
+  end
 end
