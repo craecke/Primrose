28b05634b447c0edefe635dc6597bbd8dfcf3bf8
Cody Russell <bratsche@gnome.org>
2011-06-05 00:20:08 -0500

    Cumulative quarter table (WIP)

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -1,3 +1,11 @@
 class Performer < ActiveRecord::Base
   has_many :score_lists
+
+  def quarter_average
+    s = score_lists.joins(:round).where("rounds.grouping = ?", "quarter")
+
+    a = s.collect { |x| x.average_final }
+
+    (a.inject { |sum,el| sum + el }.to_f / a.size).round(1)
+  end
 end
